946259117081611096d1d6c11a45c93bfe092c2d
Himal Sandaruwan <wahsandaruwan6@gmail.com>
2024-02-05 13:17:29 +0530

    Updated gene excel summary

--- src/api/v1/helpers/GenerateExcelFile.js
+++ src/api/v1/helpers/GenerateExcelFile.js
@@ -27,16 +27,12 @@ const GenerateExcelFile = async (columns, data, fileName, type) => {
       worksheet.addRow({
         id: item._id,
         title: item.title,
-        itemCount: item.deviceData.itemCount ? item.deviceData.itemCount : 0,
-        totalWeight: item.deviceData.totalWeight
-          ? item.deviceData.totalWeight
-          : 0,
-        batteryPercentage: item.deviceData.batteryPercentage
+        itemCount: item.deviceData ? item.deviceData.itemCount : 0,
+        totalWeight: item.deviceData ? item.deviceData.totalWeight : 0,
+        batteryPercentage: item.deviceData
           ? item.deviceData.batteryPercentage
           : 0,
-        batteryVoltage: item.deviceData.batteryVoltage
-          ? item.deviceData.batteryVoltage
-          : 0,
+        batteryVoltage: item.deviceData ? item.deviceData.batteryVoltage : 0,
       });
     });
   }
